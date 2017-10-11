8ec59cce592bbdc9d3d3b3d9497988103b7de8f8
Hack Reactor Student <student@hackreactor.com>
2017-10-11 14:44:28 -0700

    finished stack functional pattern

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    if (count > 0) {
+      count--;
+    }
+    return storage[count];
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
