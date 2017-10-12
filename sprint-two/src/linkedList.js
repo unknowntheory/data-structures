f84d935ace28f849d27163485c8e0f1a8582731f
Hack Reactor Student <student@hackreactor.com>
2017-10-12 11:34:38 -0700

    linkedList implemented

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,58 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
+  
   list.addToTail = function(value) {
+  
+    if ( list.head !== null && JSON.stringify(list.head) !== JSON.stringify(list.tail)) {
+      defineNext(list.head);
+    }  
+
+    if (list.head === null) { 
+      list.head = Node(value);
+    }
+    if (JSON.stringify(list.head) === JSON.stringify(list.tail)) {
+      list.head.next = Node(value);
+    }
+    
+    var defineNext = function (node) {
+      if (node.next === null) {
+        node.next = Node(value);
+      } 
+      if (node.next !== null) {
+        defineNext(node.next);
+      }
+    };
+
+    list.tail = Node(value);
+
   };
 
   list.removeHead = function() {
+    var output = list.head.value;
+    
+    list.head = list.head.next;
+
+    return output;
   };
 
   list.contains = function(target) {
+    var output = false;
+    var search = function(node) {
+      if (node.value === target) {
+        output = true;
+      }
+      if (node.next !== null) {
+        search(node.next);
+      }
+    };
+    
+    search(list.head);
+  
+
+
+    return output;
+ 
   };
 
   return list;
